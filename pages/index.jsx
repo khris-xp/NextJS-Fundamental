569cc24e46a580245d6dc5f0c8cf0c09350b5c8b
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-04 10:59:09 +0700

    add : data fetching

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,9 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head><title>{siteTitle}</title></Head>
@@ -10,6 +11,30 @@ export default function Home() {
         <p>Hello I'm Khris Bharmmano. I want to be a Software Developer.</p>
         <p>You can contact to me on <a href="https://www.instagram.com/khris_xp/">Instagram</a></p>
       </section>
+
+       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
